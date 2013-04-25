74b246c0f9c5d85853096f966900b7f4566e5108
Mick Soum <mick26soum@gmail.com>
2013-04-24 21:28:18 -0700

    ReadMe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toorit::Application.config.secret_token = '619fb7fa5d43920e186078db9928952009efc033d39cb590457d5d6f80420709c1d5e04c5552620a256fabef49cc4d0bb0baf667cf5a287ea869f92f97b07d32'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Toorit::Application.config.session_store :cookie_store, key: '_toorit_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Toorit::Application.config.session_store :active_record_store
